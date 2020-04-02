4a3a8b8363b954d4b7282e81d481e448a272ea24
felipe <slacklife@gmail.com>
2020-04-02 02:28:27 -0300

    atualizacao

--- Automacao/posInstalacaoDebian/posInstalacaoDebian.sh
+++ Automacao/posInstalacaoDebian/posInstalacaoDebian.sh
@@ -8,9 +8,9 @@
 #VARIAVEIS GLOBAIS
 
 DIRETORIO="pacotesPontoDeb"
-
-ADICIONAR=$(cat buscar.list)
-REMOVER=$(cat remover.list)
+DOCUMENTOS="doc"
+ADICIONAR=$(cat $DOCUMENTOS/buscar.list)
+REMOVER=$(cat $DOCUMENTOS/remover.list)
 
 
 #gnome-keyring        ---> CORRIGE O ERRO MYSQLWORKBENTCH
@@ -18,16 +18,43 @@ REMOVER=$(cat remover.list)
 #net-toos	      ---> INSTALA O IFCONFIG
 
 
-echo "EDITAR OS ARQUIVOS /etc/sudoers e /etc/apt/source.list"
+echo "EDITAR OS ARQUIVOS /etc/sudoers PARA TER ACESSO AO SUDO"
 
+###################################################################
+#COPIA UMA NOVA sources.list PARA O DIRETORIO /etc/apt/sources.list
+#
+cp $DOCUMENTOS/sources.list /etc/apt/sources.list
 
+#################################################################################
+# DOWNLOAD NECESSARIO PARA INSTALAÇÃO E ATUALIZAÇÃO DO VIRTUALBOX VIA REPOSITORIO
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+#####################
+#REMOÇÃO DE PROGRAMAS 
+#
 apt-get remove --purge $REMOVER -y
 
-dpkg -i $DIRETORIO
+###########################
+#INSTALAÇAO DE PACOTES .deb
+#
+dpkg -i $DIRETORIO/*.deb
 apt --fix-broken install
 
+#########################
+#ATUALIZACAO E INSTALAÇÃO
+#
 apt update -y
 apt upgrade -y
+
 apt-get install $ADICIONAR -y
+updatedb
+#########################
+#DOWNLOAD E INSTALAÇÃO DO msfconsole e msfvenom
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \
+  ./msfinstall
+
 m-a prepare -y
 apt autoremove -y
